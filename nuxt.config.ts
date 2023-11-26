00afd879df3ce99a9d4a806f554cfa610e5edc5d
seyun(marco) <ksy90101@gmail.com>
2023-11-26 23:12:47 +0900

    refactor: scss 도입

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,10 +6,14 @@ export default defineNuxtConfig({
     typeCheck: true,
     strict: true,
   },
+  alias: {
+    assets: '/assets',
+  },
   app: {
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
     },
   },
+  css: ['assets/styles/reset.scss'],
 });
